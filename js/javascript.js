461ec8104e05c370d33bcbdefc9fefa8846e40dc
chuonchuon9x <chuonchuon9x@gmail.com>
2020-12-29 16:44:31 +0700

    update lai code

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
+// Slide 
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -24,3 +25,16 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// Scroll
+$(window).scroll(function() { 
+  var menu = document.querySelectorAll('menu-header');
+  var menu = menu[0];
+  var scroll = pageYOffset;
+
+  if (scroll >= 100) {
+      menu.addClass("bg-header");
+  } else {
+      menu.removeClass("bg-header");
+  }
+});
